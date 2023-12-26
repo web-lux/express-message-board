0f5925f1ea172a27f9dff257b9a87ee5725f57f6
web-lux <web-lux@outlook.fr>
2023-12-26 15:20:19 +0100

    display placeholder messages in index view

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require("express");
 
 const app = express();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", { messages: messages })
 });
 
 app.listen(3000, () => {
